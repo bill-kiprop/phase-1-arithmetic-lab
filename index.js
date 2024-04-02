c94695b066d6d9e4e6d973eb03dd74347c824915
billkiprop <kipropbill3@gmail.com>
2024-04-02 09:44:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1= 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+console.log(multiply)
+
+
+let random = Math.floor(Math.random() + 1)
+console.log(random)
+
+let num3 = 16
+const num4 = 6
+let mod = (num3 %= num4)
+console.log(mod)
+
+let max = Math.max(1, 2, 3, 4, 20)
+console.log(max)
+
+
+ 
